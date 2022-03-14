5f1d410d80282e03602874d7ba28c12fd1ed5bda
AlexJBlair <alex_blair42@outlook.com>
2022-03-13 22:39:06 -0700

    Linking to mailchimp and resirecting to success and failure pages

--- app.js
+++ app.js
@@ -1,27 +1,66 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const nodemon = require('nodemon')
-const favicon = require('express-favicon')
+//jshint esversion: 6
+const express = require("express")
+const request = require("request")
+const bodyParser = require("body-parser")
+const nodemon = require("nodemon")
+const https = require("https")
+const favicon = require("express-favicon")
+const { response } = require("express")
 
 const app = express()
-const port = 3000
-
-app.use(bodyParser.json());
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
-app.get('/styles.css', (req, res) =>{
-  res.sendFile(__dirname + '/styles.css');
-})
-app.get('index.js', (req, res) =>{
-  res.sendFile(__dirname + '/index.js');
-})
-app.post('/', function(req, res) {
-  var btnResponse = req.body.btnResponse
-});
 
-app.listen(port, () => {
-  console.log('App started')
-  console.log(`Example app listening on port ${port}`)
+app.post('/', function(req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const coffeeName = req.body.coffeeName;
+  const email = req.body.email;
+  const data = {
+      members: [
+          {
+              email_address: email,
+              status: "subscribed",
+              merge_fields: {
+                  FNAME: firstName,
+                  LNAME: lastName,
+                  COFFEE: coffeeName
+              }
+          }
+      ]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us14.api.mailchimp.com/3.0/lists/570f5002b4";
+  const options = {
+      method: "POST",
+      auth: "alexb:c86823e1ea281e6cee2c78f5274e48c1-us14"
+  }
+  const request = https.request(url, options, function(response){
+      if(response.statusCode === 200)
+      {
+          res.sendFile(__dirname + "/success.html");
+      }else {
+          res.sendFile(__dirname + "/failure.html");
+      }
+
+      response.on("data", function(data){
+          console.log(JSON.parse(data));
+  })
+  });
+  request.write(jsonData);
+  request.end();
+});
+app.post("/failure", function(req, res){
+  res.redirect("/");
+})
+app.post("/success", function(req, res){
+  res.redirect("/")
+})
+
+app.listen(3000, function(){
+  console.log("App running on port 3000");
 })
